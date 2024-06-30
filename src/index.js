f63a13bb11b616357294f35388593361301bd869
April <a.d.cidoni@gmail.com>
2024-06-30 16:24:11 -0400

    phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const new_todo = document.getElementById("new-task-description").value
+      console.log(document.getElementById("new-task-description").value)
+      buildToDo(new_todo)
+      form.reset()
+    }) 
+})
+
+function buildToDo(todo) {
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    li.textContent = `${todo}`
+    li.appendChild(btn)
+    console.log(li)
+    document.getElementById('tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
